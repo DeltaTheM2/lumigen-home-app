a942ba927e700d573a632f6d685493ea2be6a920
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-07-19 02:10:36 -0700

    Home updated. Insights added

--- lib/firebase/home.dart
+++ lib/firebase/home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lumigen/components/arcProgressIndicator.dart';
 import 'package:lumigen/firebase/authentication.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,23 +15,76 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   double progress = 0.0;
   int aqi = 40;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    loadCachedData();
+  }
+
+  Future<void> loadCachedData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? cachedData = prefs.getString('homePageData');
+
+    if (cachedData != null) {
+      Map<String, dynamic> data = jsonDecode(cachedData);
+      setState(() {
+        aqi = data['aqi'];
+        progress = data['progress'];
+        isLoading = false;
+      });
+    } else {
+      fetchData();
+    }
+  }
+
+  Future<void> fetchData() async {
+    DateTime time = DateTime.now();
+    String date = DateFormat('MM-dd-yyyy').format(time);
+    int latestAqi = await AuthenticationHelper().getLastAirQuality(date);
+    double newProgress = latestAqi / 400;
+
+    setState(() {
+      aqi = latestAqi;
+      progress = newProgress;
+      isLoading = false;
+    });
+
+    saveDataToCache(latestAqi, newProgress);
+  }
+
+  Future<void> saveDataToCache(int aqi, double progress) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Map<String, dynamic> data = {
+      'aqi': aqi,
+      'progress': progress,
+    };
+    prefs.setString('homePageData', jsonEncode(data));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
+      child: isLoading
+          ? CircularProgressIndicator()
+          : Column(
         children: <Widget>[
           Card(
             margin: EdgeInsets.all(10),
-            child: Padding(padding: EdgeInsets.all(20),
+            child: Padding(
+              padding: EdgeInsets.all(20),
               child: Column(
                 children: [
                   Positioned(
-                      bottom: 8,
-                      child: ArcProgressIndicator(
-                          progress: progress,
-                          child: const Icon(Icons.air, size: 75,
-                          )
-                      )
+                    bottom: 8,
+                    child: ArcProgressIndicator(
+                      progress: progress,
+                      child: const Icon(
+                        Icons.air,
+                        size: 75,
+                      ),
+                    ),
                   ),
                   Positioned(
                     bottom: 2,
@@ -37,27 +93,24 @@ class _HomePageState extends State<HomePage> {
                       style: TextStyle(fontSize: 24),
                     ),
                   ),
-                  TextButton(onPressed: () async {
-                    DateTime time = DateTime.now();
-                    String date = DateFormat('MM-dd-yyyy').format(time);
-                    int latestAqi = await AuthenticationHelper().getLastAirQuality(date);
-                    double newProgress = latestAqi / 400;
-                    setState(() {
-                      aqi = latestAqi;
-                      progress = newProgress;
-                    });
-                  }, child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.refresh),
-                      Text("Refresh")
-                    ],
-                  ))
+                  TextButton(
+                    onPressed: () async {
+                      await fetchData();
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.refresh),
+                        Text("Refresh"),
+                      ],
+                    ),
+                  ),
                 ],
-              ),),
-          )
+              ),
+            ),
+          ),
         ],
       ),
-    );;
+    );
   }
 }
--- lib/insightsPage.dart
+++ lib/insightsPage.dart
@@ -1,14 +1,215 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-class InsightsPage extends StatefulWidget {
-  const InsightsPage({super.key});
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
+class InsightsPage extends StatefulWidget {
   @override
-  State<InsightsPage> createState() => _InsightsPageState();
+  _InsightsPageState createState() => _InsightsPageState();
 }
 
 class _InsightsPageState extends State<InsightsPage> {
+  List<FlSpot> aqiData = [];
+  bool isLoading = true;
+  String selectedTimeFrame = '7 days';
+
+  @override
+  void initState() {
+    super.initState();
+    loadCachedData();
+  }
+
+  Future<void> loadCachedData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? cachedData = prefs.getString('aqiData_$selectedTimeFrame');
+
+    if (cachedData != null) {
+      List<dynamic> decodedData = jsonDecode(cachedData);
+      setState(() {
+        aqiData = decodedData.map((item) => FlSpot(item[0], item[1])).toList();
+        isLoading = false;
+      });
+    } else {
+      fetchData();
+    }
+  }
+
+  Future<void> fetchData() async {
+    DateTime now = DateTime.now();
+    DateTime startDate;
+
+    switch (selectedTimeFrame) {
+      case '1 month':
+        startDate = now.subtract(Duration(days: 30));
+        break;
+      case 'All time':
+        startDate = DateTime(1970);
+        break;
+      case '7 days':
+      default:
+        startDate = now.subtract(Duration(days: 7));
+        break;
+    }
+
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('air-quality')
+        .where('timestamp', isGreaterThanOrEqualTo: startDate)
+        .orderBy('timestamp')
+        .get();
+
+    List<FlSpot> data = snapshot.docs.map((doc) {
+      Map<String, dynamic> airQualityData = doc['airQuality'];
+      double sum = 0;
+      int count = 0;
+      airQualityData.forEach((key, value) {
+        sum += value;
+        count++;
+      });
+      double meanAQI = sum / count;
+      return FlSpot(
+        doc['timestamp'].millisecondsSinceEpoch.toDouble(),
+        meanAQI,
+      );
+    }).toList();
+
+    setState(() {
+      aqiData = data;
+      isLoading = false;
+    });
+
+    saveDataToCache(data);
+  }
+
+  Future<void> saveDataToCache(List<FlSpot> data) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<List<double>> encodedData = data.map((spot) => [spot.x, spot.y]).toList();
+    prefs.setString('aqiData_$selectedTimeFrame', jsonEncode(encodedData));
+  }
+
+  List<LineChartBarData> getLineChartData() {
+    return [
+      LineChartBarData(
+        spots: aqiData,
+        isCurved: true,
+        barWidth: 3,
+        isStrokeCapRound: true,
+        dotData: FlDotData(show: false),
+        belowBarData: BarAreaData(
+          show: true,
+        ),
+      ),
+    ];
+  }
+
+  Widget buildChart() {
+    return LineChart(
+      LineChartData(
+        gridData: FlGridData(
+          show: true,
+          drawVerticalLine: true,
+          getDrawingHorizontalLine: (value) {
+            return FlLine(
+              color: Color(0xffe7e8ec),
+              strokeWidth: 1,
+            );
+          },
+          getDrawingVerticalLine: (value) {
+            return FlLine(
+              color: Color(0xffe7e8ec),
+              strokeWidth: 1,
+            );
+          },
+        ),
+        titlesData: FlTitlesData(
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                return Text(
+                  value.toString(),
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                );
+              },
+            ),
+          ),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                DateTime date =
+                DateTime.fromMillisecondsSinceEpoch(value.toInt());
+                return SideTitleWidget(
+                  axisSide: meta.axisSide,
+                  child: Text(
+                    DateFormat.MMMd().format(date),
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        borderData: FlBorderData(
+          show: true,
+          border: Border.all(
+            color: Color(0xffe7e8ec),
+            width: 1,
+          ),
+        ),
+        lineBarsData: getLineChartData(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Insights"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            DropdownButton<String>(
+              value: selectedTimeFrame,
+              items: <String>['7 days', '1 month', 'All time']
+                  .map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedTimeFrame = newValue!;
+                  isLoading = true;
+                  loadCachedData();
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : buildChart(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
