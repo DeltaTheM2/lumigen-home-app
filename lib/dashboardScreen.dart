9a5a198993db2f8de91924c80ebf537ff38336cc
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-08-04 21:28:34 -0700

    Pairing updated

--- lib/dashboardScreen.dart
+++ lib/dashboardScreen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:lumigen/firebase/home.dart';
+import 'package:lumigen/home.dart';
 import 'package:lumigen/insightsPage.dart';
 import 'package:lumigen/userPage.dart';
 
--- lib/deviceSettingsScreen.dart
+++ lib/deviceSettingsScreen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DeviceSettingsPage extends StatefulWidget {
   @override
@@ -11,7 +13,12 @@ class DeviceSettingsPage extends StatefulWidget {
 
 class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
   final String serverUrl = 'http://your-flask-server-ip:5000';
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   bool devicePaired = false;
+  bool isLoading = false;
+  String? deviceId;
   Color goodColor = Colors.green;
   Color moderateColor = Colors.yellow;
   Color unhealthySensitiveColor = Colors.orange;
@@ -19,10 +26,13 @@ class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
   Color veryUnhealthyColor = Colors.purple;
   Color hazardousColor = Colors.red;
 
+  final _deviceIdController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
     _loadColors();
+    _loadDeviceIdFromPrefs();
   }
 
   Future<void> _loadColors() async {
@@ -38,6 +48,44 @@ class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
 
     // Attempt to sync with the server
     await _syncWithServer();
+  }
+
+  Future<void> _loadDeviceIdFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? cachedDeviceId = prefs.getString('deviceID');
+
+    if (cachedDeviceId != null && cachedDeviceId.isNotEmpty) {
+      setState(() {
+        deviceId = cachedDeviceId;
+        devicePaired = true;
+      });
+    } else {
+      // If no cached ID, check Firestore
+      await _checkDevicePairing();
+    }
+  }
+
+  Future<void> _checkDevicePairing() async {
+    User? user = _auth.currentUser;
+    DocumentSnapshot userDoc = await _firestore.collection('users').doc(user!.uid).get();
+
+    if (userDoc.exists) {
+      Map<String, dynamic> data = userDoc.data() as Map<String, dynamic>;
+
+      if (data.containsKey('deviceID')) {
+        setState(() {
+          deviceId = data['deviceID'];
+          devicePaired = deviceId!.isNotEmpty;
+        });
+        // Cache the device ID locally
+        _saveDeviceIdToPrefs(deviceId!);
+      }
+    }
+  }
+
+  Future<void> _saveDeviceIdToPrefs(String deviceId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('deviceID', deviceId);
   }
 
   Future<void> _syncWithServer() async {
@@ -81,10 +129,30 @@ class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
   }
 
   Future<void> pairDevice() async {
-    // Logic to pair device
     setState(() {
-      devicePaired = true;
+      isLoading = true;
     });
+
+    try {
+      User? user = _auth.currentUser;
+      await _firestore.collection('users').doc(user!.uid).update({
+        'deviceID': _deviceIdController.text,
+      });
+
+      // Cache the device ID locally
+      await _saveDeviceIdToPrefs(_deviceIdController.text);
+
+      setState(() {
+        devicePaired = true;
+        deviceId = _deviceIdController.text;
+      });
+    } catch (e) {
+      print('Failed to update device ID: $e');
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 
   Future<void> setLightColor(String category, Color color) async {
@@ -147,8 +215,10 @@ class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
+        child: devicePaired
+            ? ListView(
           children: [
+            Text('Device ID: $deviceId'),
             buildColorPickerRow('Good (Green)', goodColor, (color) => setLightColor('good', color)),
             buildColorPickerRow('Moderate (Yellow)', moderateColor, (color) => setLightColor('moderate', color)),
             buildColorPickerRow('Unhealthy for Sensitive Groups (Orange)', unhealthySensitiveColor, (color) => setLightColor('unhealthySensitive', color)),
@@ -157,6 +227,23 @@ class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
             buildColorPickerRow('Hazardous (Flashing Red)', hazardousColor, (color) => setLightColor('hazardous', color)),
           ],
         )
+            : Column(
+          children: [
+            TextField(
+              controller: _deviceIdController,
+              decoration: InputDecoration(labelText: 'Enter Device ID'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: isLoading ? null : pairDevice,
+              child: isLoading
+                  ? CircularProgressIndicator(
+                color: Colors.white,
+              )
+                  : Text('Confirm Device ID'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -199,7 +286,7 @@ class ColorPickerDialog extends StatefulWidget {
 }
 
 class _ColorPickerDialogState extends State<ColorPickerDialog> {
-  Color selectedColor;
+  late Color selectedColor;
 
   _ColorPickerDialogState() : selectedColor = Colors.white;
 
